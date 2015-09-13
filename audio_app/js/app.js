09a6e07cb4bf1a6319f3d69bd474b65f7b986111
Dan Richert <dan.richert@gmail.com>
2015-09-13 18:20:18 -0400

    adds (rough) multi-channel playback/mod

--- audio_app/js/app.js
+++ audio_app/js/app.js
@@ -3,6 +3,9 @@ $(function() {
   var src  = a.createBufferSource();
   var gain = a.createGain();
 
+  var sock = new WebSocket("ws://localhost:8001");
+  var chart = null;
+
   // UI setup
   (function() {
     $("#level_slider").slider({
@@ -17,67 +20,44 @@ $(function() {
     });
   })();
 
-  var sock = new WebSocket("ws://localhost:8001");
-  var chart = null;
+  var playerBank = new PlayerBank(a, "./media/crook.mp3", function() {
+    sock.onmessage = function(msg) {
+      var data   = JSON.parse(msg.data);
+      var canvas = $("#chart")[0].getContext("2d");
 
-  sock.onmessage = function(msg) {
-    var data   = JSON.parse(msg.data);
-    var canvas = $("#chart")[0].getContext("2d");
+      var datasets = [{ data: data, fillColor: "#abc" }];
 
-    var datasets = [{ data: data, fillColor: "#abc" }];
+      var labels = Array.apply(null, Array(200)).map(
+        String.prototype.valueOf, "");
 
-    var labels = Array.apply(null, Array(200)).map(
-      String.prototype.valueOf, "");
+      if(!chart) {
+        chart = new Chart(canvas).Bar({
+          labels:   labels,
+          datasets: datasets
+        }, {
+          animation:                true,
+          animationSteps:           10,
+          scaleOverride:            true,
+          scaleStartValue:          0,
+          scaleStepWidth:           64,
+          scaleSteps:               16,
+          barShowStroke:            false,
+          barDataSetSpacing:        3,
+          scaleShowVerticalLines:   false,
+          scaleShowHorizontalLines: false
+        });
+      }
+      else {
+        data.forEach(function(n, ndx) {
+          chart.datasets[0].bars[ndx].value = n;
+        });
 
-    if(!chart) {
-      chart = new Chart(canvas).Bar({
-        labels:   labels,
-        datasets: datasets
-      }, {
-        animation:                true,
-        animationSteps:           10,
-        scaleOverride:            true,
-        scaleStartValue:          0,
-        scaleStepWidth:           64,
-        scaleSteps:               16,
-        barShowStroke:            false,
-        barDataSetSpacing:        3,
-        scaleShowVerticalLines:   false,
-        scaleShowHorizontalLines: false
-      });
-    }
-    else {
-      data.forEach(function(n, ndx) {
-        chart.datasets[0].bars[ndx].value = n;
-      });
+        chart.update();
+      }
 
-      chart.update();
-    }
-  };
-
-  // context - AudioContext
-  (function(context) {
-    var req = new XMLHttpRequest();
-
-    req.open("GET", "./media/crook.mp3", true);
-
-    req.responseType = "arraybuffer";
-
-    req.onload = function() {
-      context.decodeAudioData(req.response, function(buf) {
-        src.buffer = buf;
-
-        var modBank = new ModBank(context);
-
-        src.connect(gain);
-        gain.connect(context.destination);
-
-        src.start(0);
-      }, null);
-    }
-
-    req.send();
-  })(a);
+      playerBank.update(data);
+    };
+  });
 });
 
 var ModBank = function(audioContext) {
@@ -96,15 +76,69 @@ var ModBank = function(audioContext) {
     this.nodes.forEach(function(node, ndx) {
       var freq = freqs[ndx] * that.maxFreq + that.minFreq;
 
-      node.exponentialRampToValueAtTime(freq,
-        context.currentTime + 0.001);
+      node.frequency.value = freq;
+      //node.frequency.exponentialRampToValueAtTime(freq,
+      //  that.context.currentTime + 0.001);
+
+      node.start();
     });
   };
 
   (function() {
     for(var i = 0; i < this.numNodes; i++) {
-      this.nodes[i] = audioContext.createOscillator();
+      this.nodes[i] = this.context.createOscillator();
       this.nodes[i].frequency.value = this.minFreq;
     }
+  }).call(this);
+};
+
+var PlayerBank = function(audioContext, path, cbk) {
+  this.path = path;
+  this.context = audioContext;
+  this.callback = cbk;
+
+  this.numNodes = 200;
+  this.nodes    = [];
+
+  this.position = 0; // sec?
+  this.rate     = 1; // ???
+
+  // data - Array from data source; should be an array of 200 0..1024 numbers
+  this.update = function(data) {
+    // convert to an array of 0..1 values
+    var scalars = data.map(function(n) { return n / 1024.0; });
+
+    this.modBank.setFreqs(scalars);
+
+    for(var i = 0; i < this.numNodes; i++) {
+      this.modBank.nodes[i].connect(this.nodes[i].playbackRate);
+    }
+  };
+
+  (function() {
+    var that = this;
+
+    this.modBank = new ModBank(this.context);
+
+    var req = new XMLHttpRequest();
+
+    req.open("GET", that.path, true);
+    req.responseType = "arraybuffer";
+
+    req.onload = function() {
+      that.context.decodeAudioData(req.response, function(buf) {
+        for(var i = 0; i < that.numNodes; i++) {
+          that.nodes[i] = that.context.createBufferSource();
+          that.nodes[i].buffer = buf;
+
+          that.nodes[i].connect(that.context.destination);
+          that.nodes[i].start();
+        }
+      }, null);
+
+      that.callback();
+    }
+
+    req.send();
   }).call(this);
 };
