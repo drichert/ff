f6fb0a6a15945b359a79d57ca15654b430ec6b4b
Dan Richert <dan.richert@gmail.com>
2015-09-23 23:49:39 -0400

    receiving data in pd

--- websocket_server/app.js
+++ websocket_server/app.js
@@ -1,20 +1,32 @@
 var serialport = require("serialport");
-var websocket  = require("websocket");
+//var websocket  = require("websocket");
 var http       = require("http");
+var osc        = require("node-osc");
+//var osc = require("osc-min");
+//var udp = require("dgram");
 
 var port = 8181;
 
 var server = new http.createServer();
-var socket = new websocket.server({ httpServer: server });
+//var socket = new websocket.server({ httpServer: server });
 
-var stack = []
-var clients = [];
+//var stack = []
+
+var oscClient = new osc.Client("127.0.0.1", 57777);
 
 var parseData = function(data) {
-  return data.split(",").slice(0, -1).filter(function(val, ndx) {
+  data = data.split(",").slice(0, -1).filter(function(val, ndx) {
     return ndx % 2;
+  }).map(function(n) {
+    return parseFloat(n);
   });
+
+  return data;
 };
+
+//var parseData = function(data) {
+//  return data.split(",").slice(0, -1);
+//};
 
 var serial = new serialport.SerialPort("/dev/ttyACM0", {
   baudrate: 115200,
@@ -27,15 +39,17 @@ serial.on("open", function() {
   console.log("Serial Port Opened");
 
   serial.on("data", function(data){
-    var json = JSON.stringify(parseData(data));
+    //var json = JSON.stringify(parseData(data));
 
-    if(connection) connection.send(json);
+    //if(connection) connection.send(json);
+    //oscClient.send("/data", json);
+    oscClient.send("/data", parseData(data));
   });
 });
 
-socket.on("request", function(request) {
-  connection = request.accept(null, request.origin);
-});
+//socket.on("request", function(request) {
+//  connection = request.accept(null, request.origin);
+//});
 
 server.listen(port, function() {
   console.log("Server listening on port " + port);
