5204bbaf0c1d1f032ccfd326c8eedbd8685ac0db
Dan Richert <dan.richert@gmail.com>
2015-09-28 23:24:54 -0400

    Adds zone_avgs

--- ruby_serialport_server/server.rb
+++ ruby_serialport_server/server.rb
@@ -1,7 +1,7 @@
 require "serialport"
 require "osc-ruby"
 
-config = { arduino_serial: "/dev/ttyACM0", 
+config = { arduino_serial: "/dev/ttyACM0",
   baud_rate: 115200 }
 
 client = OSC::Client.new("192.168.0.107", 57777)
@@ -12,18 +12,33 @@ def parse_data(data)
   data.split(",").select.with_index {|_, ndx| ndx.odd? }.map(&:to_f)
 end
 
+def zone_avgs(data, num_zones = 5)
+  zone_size = data.size / num_zones
+
+  avgs = []
+  num_zones.times do |n|
+    avgs << (zone = data[(start = zone_size * n)..(start + zone_size - 1)])
+      .reduce(:+) / zone.size
+  end
+  avgs
+end
+
 while true do
   while(line = serial.gets) do
-    floats = parse_data(line)
-    avg    = floats.inject(:+) / floats.size
+    floats   = parse_data(line)
+    avg      = floats.inject(:+) / floats.size
+    zavgs    = zone_avgs(floats)
 
     msgs = {
-      data: OSC::Message.new("/data", *floats),
-      avg:  OSC::Message.new("/avg", avg)
+      data:      OSC::Message.new("/data", *floats),
+      avg:       OSC::Message.new("/avg", avg),
+      zone_avgs: OSC::Message.new("/zone_avgs", *zavgs)
     }
 
-    puts "Average: #{avg}"
+    puts "Average #{avg}"
+    puts "Zone Averages #{zavgs}"
 
     msgs.each {|k, v| client.send(v) }
   end
 end
+
